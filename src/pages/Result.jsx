9cb60cd46cd4fae942067a17e3d34217b95b7e31
rajchoudhary6056 <rajchoudhary6056@gmail.com>
2026-07-03 23:23:42 +0530

    h

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -3,12 +3,16 @@ import "./Result.css";
 export default function Result() {
   return (
     <div className="result-page">
+
       <div className="paper">
+
         <img
           src="/kanha.jpeg"
           alt="Result"
         />
+
       </div>
+
     </div>
   );
 }
